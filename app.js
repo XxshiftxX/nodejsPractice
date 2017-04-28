5259a6bc7dd5a7242f67b82743ff0c35aac3c597
shift <qjfrntop12@naver.com>
2017-04-28 10:55:05 +0900

    static 폴더 세팅 하는데 오류...

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello, World!');
+app.set("view engine", 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.render('my_first_ejs');
 });
 
 app.listen(1000, function(){
